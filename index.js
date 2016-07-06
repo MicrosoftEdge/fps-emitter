bdcf0322aed09d46bdadd54420ecabbc337f2910
Nolan Lawson <nolan.lawson@gmail.com>
2016-07-06 11:36:04 -0700

    fix license format

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-﻿//--------------------------------------------------------------
+﻿// --------------------------------------------------------------
 //
 // Microsoft Edge fps-emitter
 // Copyright(c) Microsoft Corporation
@@ -23,7 +23,7 @@
 // WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
 // OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-//--------------------------------------------------------------
+// --------------------------------------------------------------
 'use strict'
 
 /* global performance */
